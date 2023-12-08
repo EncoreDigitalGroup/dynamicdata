34569a62e25ac1eb9913c5c38abae5d2cee112fc
Marc Beinder <marc@marcbeinder.com>
2023-12-08 13:08:19 -0600

    updated composer.json

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EncoreDigitalGroup\PackageName;
+namespace EncoreDigitalGroup\DynamicData;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -15,7 +15,7 @@ class ServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('package-name')
+            ->name('dynamicdata')
             ->hasConfigFile();
     }
 
